3b4423f28d74ce21e52f95160f6025c3945bc258
Oliver Thiele <mail@oliver-thiele.de>
2025-12-01 16:29:16 +0100

    [FEATURE] Updates description and increments version
    Enhances extension description to clarify functionality.
    Increments the version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,13 +2,13 @@
 
 $EM_CONF['ot_sitekitceimgtextoverlay'] = [
     'title' => 'CE Image with text overlay',
-    'description' => '',
+    'description' => '"TYPO3 content element that displays an image with text overlay. Integrates with the Sitekit and ot-irrebuttons extensions.',
     'category' => 'frontend',
     'author' => 'Oliver Thiele',
     'author_email' => 'mail@oliver-thiele.de',
     'author_company' => 'Web Development Oliver Thiele',
     'state' => 'stable',
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'constraints' =>
         [
             'depends' =>
